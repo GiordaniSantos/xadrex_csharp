02034c39506d842686ef241086a4f49e2b4bc72c
Giordani da Silveira dos Santos <dani.santossilveira@hotmail.com>
2022-12-17 15:59:25 -0300

    Criando a classe posicao

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Xadrez
+﻿using tabuleiro;
+
+namespace Xadrez
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Posicao P;
+
+            P = new Posicao(3,4);
+
+            Console.WriteLine(P);
+            Console.ReadLine();
         }
     }
 }
